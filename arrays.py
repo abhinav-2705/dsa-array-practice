53a14a0cba645428d70d2dc55377c6995858aaf8
Abhinav <raj316638@gmail.com>
2026-05-05 13:25:32 +0530

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,11 @@
+def find_max(arr):
+    return max(arr)
+
+def reverse_array(arr):
+    return arr[::-1]
+
+def sum_array(arr):
+    return sum(arr)
+
+print(find_max([1, 5, 3]))
+print(reverse_array([1, 2, 3]))
